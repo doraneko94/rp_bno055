07a08970a87e43c2846e8e1886ae55cb7fa3da7f
doraneko94 <shuntaro94@gmail.com>
2023-05-01 06:57:54 +0900

    add serial_hello

---
+++ src/serial_hello.rs
@@ -0,0 +1,72 @@
+//! Serial Example.
+
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use rp2040_hal as hal;
+use hal::pac;
+
+use embedded_hal::digital::v2::OutputPin;
+use rp2040_hal::clocks::Clock;
+
+use usb_device::{class_prelude::*, prelude::*};
+use usbd_serial::SerialPort;
+
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[rp2040_hal::entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    // Set the USB bus
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    // Set the serial port
+    let mut serial = SerialPort::new(&usb_bus);
+
+    // Set a USB device
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(2)
+        .build();
+
+    loop {
+        for _ in 0..200 {
+            delay.delay_ms(5);
+            let _ = usb_dev.poll(&mut [&mut serial]);
+        }
+        let _ = serial.write(b"Hello, world!\r\n");
+    }
+}
+
+// End of file
